26745e2ff876b1036e84bc543497dab85d7a6ccc
PereiraKauan <kauanpsantos2@gmail.com>
2026-05-13 11:44:51 -0300

    finishing tutorial03

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,29 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, word. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context, request))
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    response = "Esses são os resultado da pesquisa %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Obrigado por nós ajudar na pesquisa %s." % question_id)
